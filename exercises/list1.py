aa94d88efce6f7433ae2117bd3df452736c7753d
rafaelandpires <rafdesenv@gmail.com>
2015-02-07 23:08:38 -0200

    x

--- exercises/list1.py
+++ exercises/list1.py
@@ -1,4 +1,12 @@
 #!/usr/bin/python -tt
+# Copyright 2010 Google Inc.
+# Licensed under the Apache License, Version 2.0
+# http://www.apache.org/licenses/LICENSE-2.0
+
+# Google's Python Class
+# http://code.google.com/edu/languages/google-python-class/
+
+# Additional basic list exercises
 
 # A. match_ends
 # Given a list of strings, return the count of the number of
